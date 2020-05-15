b730ff2b035ce3267ff698ed95ebb2e34c7e56b1
Ian Bastos <ian-bastos@live.com>
2020-05-15 23:19:05 +0100

    wrapper for native code in javascript for linting

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 //index.js
-const electronScreenshot = require('./build/Release/electron-screenshot.node');
+const electronScreenshotNative = require('./build/Release/electron-screenshot.node');
+
+const electronScreenshot = {
+
+    /**
+     * Take a screenshot of the current screen
+     * @return {String} Base64 encoded png image of the screen buffer
+     */
+    takeScreenshot: function() {
+        return electronScreenshotNative.takeScreenshot();
+    }
+
+}
 
 module.exports = electronScreenshot;
